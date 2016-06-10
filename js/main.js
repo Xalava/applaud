52e698e1a6d884ec5c9d9a1f583642f1d94c7f53
xalava <xavier.lavayssiere@gmail.com>
2016-06-10 16:07:02 +0200

    blabla

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
+var valeurPrec = 250;
+
 setInterval(
 	function(){ 
 		$.get("https://iot.seeed.cc/v1/node/GenericAInA0/analog?access_token=7c73fb1b4aa98f284dc0601a855b045c", function(data, status){
         	// alert("Data: " + data + "\nStatus: " + status);
+        	var valeur = Math.round((valeurPrec + data.analog)/4);
 
-        	var valeurText = data.analog.toString();
+        	var valeurText = valeur.toString();
 			$(".volume").html(valeurText);
 
-			var valeurPercent = data.analog/500*100
+			var valeurPercent = valeur;
 		
 			$('.progress-bar').css('width', valeurPercent+'%');
+
+			valeurPrec = valeur;
 
     	});
 			
